4afff6a3292d09337587b0b1882e2f26bac39c6f
Giordano Cardillo <giordano.cardillo@unipegaso.it>
2016-06-30 17:54:08 +0200

    Exported db configuration to JSON

--- app/utils/DBLink.php
+++ app/utils/DBLink.php
@@ -2,32 +2,33 @@
 
 class ConnectionDetails {
 
-	public $driver, $host, $dbName, $username, $password;
+    public $driver, $host, $dbName, $username, $password;
 
-	public function __construct($driver, $host, $dbName, $username, $password) {
-		$this->driver = $driver;
-		$this->host = $host;
-		$this->dbName = $dbName;
-		$this->username = $username;
-		$this->password = $password;
-	}
+    public function __construct($driver, $host, $dbName, $username, $password) {
+        $this->driver = $driver;
+        $this->host = $host;
+        $this->dbName = $dbName;
+        $this->username = $username;
+        $this->password = $password;
+    }
 }
 
 class DBLink extends PDO {
 
-	private $connections = [];
+    private $connections = [];
 
-	public function __construct($connection) {
+    public function __construct($connection) {
 
-		$this->connections['db'] = new ConnectionDetails('mysql', '', '', '', '');
+        // Getting DB configurations
+        $this->connections = json_decode(file_get_contents(APP . "/db.config.json"));
 
-		if (!array_key_exists($connection, $this->connections)){
-			throw  new Exception("Database configuration does not exist");
-		}
+        if (!isset($this->connections->{$connection})) {
+            throw  new Exception("Database configuration does not exist");
+        }
 
-		$connectionDetails = $this->connections[$connection];
+        $connectionDetails = $this->connections->{$connection};
 
-		$dsn = "$connectionDetails->driver:host=$connectionDetails->host;dbname=$connectionDetails->dbName";
-		return parent::__construct($dsn, $connectionDetails->username, $connectionDetails->password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	}
+        $dsn = "$connectionDetails->driver:host=$connectionDetails->host;dbname=$connectionDetails->dbName";
+        return parent::__construct($dsn, $connectionDetails->username, $connectionDetails->password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    }
 }
