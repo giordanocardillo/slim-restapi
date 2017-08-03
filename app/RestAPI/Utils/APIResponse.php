b88473caf405eb22936032df7395cdbcc0cc760c
Giordano Cardillo <giordano.cardillo@gmail.com>
2017-08-03 09:47:30 +0200

    Added camelCaseKeys utility function to convert array keys to camelCase.

--- app/RestAPI/Utils/APIResponse.php
+++ app/RestAPI/Utils/APIResponse.php
@@ -78,4 +78,27 @@ class APIResponse {
     return $response->withJson(["data" => $data], $status);
   }
 
+  private static function camelCaseKeys($array, $arrayHolder = array()) {
+
+    $camelCaseArray = !empty($arrayHolder) ? $arrayHolder : array();
+
+    foreach ($array as $key => $val) {
+      $newKey = @explode('_', $key);
+      array_walk($newKey, create_function('&$v', '$v = ucwords($v);'));
+      $newKey = @implode('', $newKey);
+      $newKey{0} = strtolower($newKey{0});
+
+      if (is_object($val)) {
+        $val = (array)$val;
+      }
+
+      if (!is_array($val)) {
+        $camelCaseArray[$newKey] = $val;
+      } else {
+        $camelCaseArray[$newKey] = self::camelCaseKeys($val, $camelCaseArray[$newKey]);
+      }
+    }
+    return $camelCaseArray;
+  }
+
 }
